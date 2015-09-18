0553653ec1fc4029bb9545c74faba22f00ff4ad1
jfr3000 <julia.friesel@gmail.com>
2015-09-18 17:55:26 +0200

    allow optional alternative id provision. fix tests.

--- renderer.js
+++ renderer.js
@@ -2,7 +2,8 @@ var uuid = require('uuid');
 var md     = require('markdown-it');
 var Plugin = require('markdown-it-regexp');
 
-function makePlugin(onAnswerOption) {
+function makePlugin(onAnswerOption, opts) {
+    opts = opts || {};
 	return Plugin(
 	  // regexp to match
 	  /\[([\sx])\]/,
@@ -10,15 +11,16 @@ function makePlugin(onAnswerOption) {
 	  // this function will be called when something matches
 	  function(match, utils) {
 		var checked = match[1] === 'x';
-		var id = uuid.v4();
+		var id = opts.getId ? opts.getId() : uuid.v4();
+        console.log(opts);
 		onAnswerOption(id, checked);
 		return '<input id="' + id + '" type="checkbox">';
 	  }
 	);
 }
 
-module.exports = function render(input, optionsHandler) {
+module.exports = function render(input, optionsHandler, opts) {
 	return md()
-		.use(makePlugin(optionsHandler))
+		.use(makePlugin(optionsHandler, opts))
 		.render(input);
 };
--- test/test.js
+++ test/test.js
@@ -1,13 +1,32 @@
 var test = require("tape");
 var renderer = require("../renderer");
-
+var opts = {
+    getId: function() {
+        return "42";
+    }
+};
 test("should convert [x] to an checked HTML checkbox element", function(t){
-	var html = renderer('- [x] hello').replace(/\n/g, '');	
-	t.equal(html, '<ul><li><input type="checkbox" checked> hello</li></ul>');
+	var html = renderer('- [x] hello', function(){}, opts).replace(/\n/g, '');
+	t.equal(html, '<ul><li><input id="42" type="checkbox"> hello</li></ul>');
 	t.end();
 });
 test("should convert [ ] to an unchecked HTML checkbox element", function(t){
-	var html = renderer('- [ ] hello').replace(/\n/g, '');	
-	t.equal(html, '<ul><li><input type="checkbox"> hello</li></ul>');
+	var html = renderer('- [ ] hello', function(){}, opts).replace(/\n/g, '');
+	t.equal(html, '<ul><li><input id="42" type="checkbox"> hello</li></ul>');
 	t.end();
+});
+
+test("should call onAnswerOption with question id and its correctness", function(t){
+	renderer('- [ ] hello', function(id, correct){
+        t.equal(id, "42");
+        t.equal(correct, false);
+        t.end();
+    }, opts);
+});
+test("should call onAnswerOption with question id and its correctness", function(t){
+	renderer('- [x] hello', function(id, correct){
+        t.equal(id, "42");
+        t.equal(correct,true);
+        t.end();
+    }, opts);
 });
